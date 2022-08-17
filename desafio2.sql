699683475dbb02827a716ff05d70941927741cf8
Otávio Azevedo <otavioad1997@gmail.com>
2022-08-17 17:50:57 -0300

    Requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,8 @@
+SELECT 
+  COUNT(SpotifyClone.songs.song) AS cancoes,
+  (SELECT COUNT(artist_name) FROM SpotifyClone.artist) AS artistas,
+  (SELECT COUNT(album_name) FROM SpotifyClone.album) AS albuns
+FROM 
+  SpotifyClone.songs
+INNER JOIN SpotifyClone.album AS a ON a.album_id = SpotifyClone.songs.album_id
+INNER JOIN SpotifyClone.artist AS art ON a.artist_id = art.artist_id;
