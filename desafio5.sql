8e261d382dd9f63c939d488bceabf71c814dd00e
Otávio Azevedo <otavioad1997@gmail.com>
2022-08-18 12:57:26 -0300

    Requisito 5 - ATT

--- desafio5.sql
+++ desafio5.sql
@@ -1,14 +1,13 @@
--- SELECT 
--- song AS cancao,
--- COUNT(song) AS reproducoes
--- FROM SpotifyClone.history 
--- GROUP BY song 
--- ORDER BY COUNT(song) DESC LIMIT 2;
-
 SELECT 
 song AS cancao,
 COUNT(song) AS reproducoes
 FROM SpotifyClone.history 
 GROUP BY song 
-HAVING COUNT(song) > 2
+HAVING COUNT(song) >= (
+    SELECT 
+    COUNT(song)
+    FROM SpotifyClone.history 
+    GROUP BY song 
+    ORDER BY COUNT(song) DESC LIMIT 1
+)
 ORDER BY song ASC;
