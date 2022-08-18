538662892db6a0497c6c0155dcd7e69317578220
Otávio Azevedo <otavioad1997@gmail.com>
2022-08-18 17:16:14 -0300

    Requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,17 @@
+-- Active: 1660757313270@@127.0.0.1@3307@SpotifyClone
+SELECT art.artist_name AS artista, 
+a.album_name AS album, 
+(SELECT COUNT(user_id) FROM SpotifyClone.followers AS f WHERE f.artist_id = art.artist_id) AS seguidores
+FROM SpotifyClone.album AS a
+INNER JOIN SpotifyClone.artist AS art ON art.artist_id = a.artist_id
+ORDER BY 3 DESC, art.artist_name, a.album_name;
+
+-- Steps
+-- SELECT * FROM SpotifyClone.artist;
+
+-- SELECT * FROM SpotifyClone.followers;
+
+-- SELECT * FROM SpotifyClone.album;
+
+-- SELECT COUNT(user_id) FROM SpotifyClone.followers
+-- GROUP BY artist_name, artist_id;
